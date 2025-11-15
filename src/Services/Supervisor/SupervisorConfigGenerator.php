f729c5d27185e729f50cc6e201a24db9c224bbaa
Piotr Adamczyk <admin@adamczykpiotr.com>
2025-11-15 13:26:34 +0100

    fix: phpstan errors

--- src/Services/Supervisor/SupervisorConfigGenerator.php
+++ src/Services/Supervisor/SupervisorConfigGenerator.php
@@ -7,38 +7,43 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 
-class SupervisorConfigGenerator
-{
+class SupervisorConfigGenerator {
+
     public function __construct(
         protected CpuCoreConfigurationResolver $cpuCoreResolver,
-    ) {}
+    ) {
+    }
 
-    public function generate(): string
-    {
-        /** @var Collection<string, array<string, mixed>> $config */
+
+    public function generate(): string {
+        /** @var Collection<string, array<string|int, mixed>> $config */
         $config = collect((array) config('balanced-queues'));
 
-        $workerOptions = $this->buildWorkerOptionString(
-            collect($config->get('worker_options', []))
-        );
 
-        $queues = collect($config->get('queues', []))
-            ->map(fn (float|int $coreCount, string $workloadType) => $this->generateConfigEntry(
+        /** @var Collection<int|string, string|int> $workerOptions */
+        $workerOptions = collect($config->get('worker_options', []));
+        $workerOptionString = $this->buildWorkerOptionString($workerOptions);
+
+        /** @var Collection<string, float|int> $queueOptions */
+        $queueOptions = $config->get('queues', []);
+        $queues = collect($queueOptions)
+            ->map(fn(float|int $coreCount, string $workloadType) => $this->generateConfigEntry(
                 $workloadType,
                 max($this->cpuCoreResolver->resolveCpuCores($coreCount), 1),
-                $workerOptions
+                $workerOptionString
             ))
             ->join("\n\n");
 
-        $header = $this->buildSupervsorHeader(
-            collect($config->get('supervisor.header', []))
-        );
 
-        return "$header\n\n$queues\n";
+        /** @var Collection<string, array<string, string|int|float>|null> $headerOptions */
+        $headerOptions = collect($config->get('supervisor.header', []));
+        $headerString = $this->buildSupervisorHeader($headerOptions);
+
+        return "$headerString\n\n$queues\n";
     }
 
-    protected function generateConfigEntry(string $workloadType, int $coreCount, string $optionString): string
-    {
+
+    protected function generateConfigEntry(string $workloadType, int $coreCount, string $optionString): string {
         $path = base_path("storage/logs/queue/{$workloadType}");
         if (File::exists($path) === false) {
             File::makeDirectory($path, recursive: true);
@@ -58,22 +63,22 @@ stdout_logfile={$path}/%(process_num)03d.log
 stderr_logfile={$path}/%(process_num)03d_error.log";
     }
 
+
     /**
-     * @param  Collection<string, array<string, string|int|float>|null>  $header
+     * @param Collection<string, array<string, string|int|float>|null> $header
      */
-    protected function buildSupervsorHeader(Collection $header): string
-    {
+    protected function buildSupervisorHeader(Collection $header): string {
         $requiredSectionName = 'supervisord';
         if ($header->has($requiredSectionName) === false) {
             $header->put($requiredSectionName, []);
         }
 
-        $groups = $header->map(function (?array $items, string $groupName) {
+        $groups = $header->map(function(?array $items, string $groupName) {
             if ($items === null) {
                 return null;
             }
 
-            $items = collect($items)->map(fn ($value, $key) => "$key=$value")->implode("\n");
+            $items = collect($items)->map(fn($value, $key) => "$key=$value")->implode("\n");
 
             return "[$groupName]\n$items";
         });
@@ -81,11 +86,15 @@ stderr_logfile={$path}/%(process_num)03d_error.log";
         return $groups->filter()->implode("\n\n");
     }
 
-    protected function buildWorkerOptionString(Collection $options): string
-    {
+
+    /**
+     * @param Collection<int|string, mixed> $options
+     * @return string
+     */
+    protected function buildWorkerOptionString(Collection $options): string {
         return $options
             ->forget(['queue'])
-            ->map(function (mixed $value, string|int $key) {
+            ->map(function(mixed $value, string|int $key) {
                 $isFlag = is_int($key);
                 if ($isFlag) {
                     return (string) $value;
@@ -93,19 +102,19 @@ stderr_logfile={$path}/%(process_num)03d_error.log";
 
                 return "$key=$value";
             })
-            ->filter(fn (mixed $value) => Str::startsWith($value, '--'))
+            ->filter(fn(mixed $value) => Str::startsWith($value, '--'))
             ->implode(' ');
     }
 
-    protected function getPhpExecutable(): string
-    {
+
+    protected function getPhpExecutable(): string {
         return defined('PHP_BINARY')
             ? PHP_BINARY
             : 'php';
     }
 
-    protected function getArtisanPath(): string
-    {
+
+    protected function getArtisanPath(): string {
         return base_path('artisan');
     }
 }
