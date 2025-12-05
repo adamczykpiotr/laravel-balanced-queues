2b244c9e8c5061a9cf01128e25fe93a96bfb47bb
Piotr Adamczyk <admin@adamczykpiotr.com>
2025-12-05 16:35:42 +0100

    feat: introduce new queue types

--- src/Enums/JobWorkloadType.php
+++ src/Enums/JobWorkloadType.php
@@ -2,12 +2,15 @@
 
 namespace AdamczykPiotr\LaravelBalancedQueues\Enums;
 
-enum JobWorkloadType: string
-{
+enum JobWorkloadType: string {
+
     case DEFAULT = 'default';
+
+    case FILESYSTEM = 'filesystem';
 
     case CPU_HIGH = 'cpu-high';
     case CPU_MEDIUM = 'cpu-medium';
+    case CPU_LOW = 'cpu-low';
 
     case NETWORK_HIGH_BANDWIDTH = 'network-high-bandwidth';
     case NETWORK_HIGH_REQUESTS = 'network-high-requests';
