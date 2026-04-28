0fc2f9c20e467e0a0b2a17e4b29fe1ea40ecc6fb
adamczykpiotr <20736260+adamczykpiotr@users.noreply.github.com>
2026-04-28 15:12:30 +0000

    style: format code

--- src/Traits/HasBalancedQueues.php
+++ src/Traits/HasBalancedQueues.php
@@ -5,51 +5,52 @@ namespace AdamczykPiotr\LaravelBalancedQueues\Traits;
 use AdamczykPiotr\LaravelBalancedQueues\Enums\JobWorkloadType;
 use Illuminate\Bus\Queueable;
 
-trait HasBalancedQueues {
-
+trait HasBalancedQueues
+{
     use Queueable;
 
-    public function onDefaultQueue(): static {
+    public function onDefaultQueue(): static
+    {
         return $this->onQueue(JobWorkloadType::DEFAULT->value);
     }
 
-
-public function onFilesystemQueue(): static
+    public function onFilesystemQueue(): static
     {
         return $this->onQueue(JobWorkloadType::FILESYSTEM->value);
     }
 
-    public function onFilesystemQueue(): static {
+    public function onFilesystemQueue(): static
+    {
         return $this->onQueue(JobWorkloadType::FILESYSTEM->value);
     }
 
-
-    public function onHighCpuUsageQueue(): static {
+    public function onHighCpuUsageQueue(): static
+    {
         return $this->onQueue(JobWorkloadType::CPU_HIGH->value);
     }
 
-
-    public function onMediumCpuUsageQueue(): static {
+    public function onMediumCpuUsageQueue(): static
+    {
         return $this->onQueue(JobWorkloadType::CPU_MEDIUM->value);
     }
 
-
-public function onLowCpuUsageQueue(): static
+    public function onLowCpuUsageQueue(): static
     {
         return $this->onQueue(JobWorkloadType::CPU_LOW->value);
     }
 
-    public function onLowCpuUsageQueue(): static {
+    public function onLowCpuUsageQueue(): static
+    {
         return $this->onQueue(JobWorkloadType::CPU_LOW->value);
     }
 
-
-    public function onHighNetworkRequestUsageQueue(): static {
+    public function onHighNetworkRequestUsageQueue(): static
+    {
         return $this->onQueue(JobWorkloadType::NETWORK_HIGH_REQUESTS->value);
     }
 
-
-    public function onHighNetworkBandwidthUsageQueue(): static {
+    public function onHighNetworkBandwidthUsageQueue(): static
+    {
         return $this->onQueue(JobWorkloadType::NETWORK_HIGH_BANDWIDTH->value);
     }
 }
